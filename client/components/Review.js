4af7c393cbe6e065c33db146c5507ed00c190336
Max Volovikov <maxvolovikov@gmail.com>
2018-11-03 02:08:46 -0500

    layout is complete and the testing is performed for each component

--- client/components/Review.js
+++ client/components/Review.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import Stars from './Stars'
-import Controls from './Controls'
-import moment from 'moment'
+import Stars from './Stars';
+import TextBody from './Body';
+import Controls from './Controls';
+import moment from 'moment';
 
   const oneReviewRrapper = {
     borderBottom: '1px solid grey',
@@ -22,15 +23,27 @@ import moment from 'moment'
   const leftColumn = {
     display: 'inline-block',
     width: '300px',
+    height:'100px',
+    fontSize: '15px',
+  }
+  const emptySpace = {
+    display: 'inline-block',
+    width: '300px',
+    height:'100px',
   }
 
   const rightColumn = {
     display: 'inline-block',
-    maxWidth: '100%',
-    paddingLeft: '1%'
+    maxWidth: '70%',
+    paddingLeft: '1%',
+    height: 'auto', 
+    lineHeight:'1.43',
+    fontSize: '15px',
+    verticalAllign:'top',
   }
 
   const avatarContainer = {
+    overflow: 'auto',
     display: 'block',
     width: '29%',
     maxHeight: '100%',
@@ -76,10 +89,11 @@ import moment from 'moment'
       <span className="time" style={time}>{ moment(review.created).fromNow()}</span>
       <span className="author" style={author}>{review.author}</span>
       </div>
+      <div className="emptySpace" style={emptySpace}></div>
     </div>
     <div className="rigth-column" style={rightColumn}>
       <div className="stars-container" style={raitings}><Stars stars={review.stars}/></div>
-      <div className="text-container">{review.body}</div>
+      <div className="text-container" ><TextBody text={review.body}/></div>
       <div className="controls-container"><Controls /></div>
     </div>
   </div>
